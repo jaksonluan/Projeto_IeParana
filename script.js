025f8aad7baf2717d36d1d9e0690fd0412b486bd
Jakson Luan <jaaksonluan@hotmail.com>
2025-02-19 14:06:55 -0300

    Adiciona responsividade à sectCAP e menu-toggle às páginas

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navbar = document.querySelector('.navbar');
+menuToggle.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
